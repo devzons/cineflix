d9bf43d35c6790ea7dd1a90f2930ccefc2ca734e
Don Yu <37088038+devzons@users.noreply.github.com>
2023-04-18 01:03:09 -0700

    added dark mode

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -27,7 +27,7 @@ const Profile = () => {
   useEffect(() => {
     refetchFavorites()
     refetchWatchlisted()
-  }, [])
+  })
 
   const logout = () => {
     localStorage.clear()
--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { Provider } from 'react-redux'
+
+import ToggleColorMode from './utils/ToggleColorMode'
 import './index.css'
 import App from './App'
 import store from './app/store'
 
-const theme = createTheme({})
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
+    <ToggleColorMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ThemeProvider>
+    </ToggleColorMode>
   </Provider>
 )
